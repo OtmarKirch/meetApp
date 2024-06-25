b6af0601a3bfcebea132fa69caa408636c116ccc
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-06-25 14:24:57 +0200

    Enable offline use

--- src/api.js
+++ src/api.js
@@ -36,6 +36,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -44,6 +50,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
