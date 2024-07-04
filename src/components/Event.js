13d44d8741192f9315fb6c2c03f08cee8fd19533
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-07-04 07:18:31 +0200

    Debug show details button

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,27 +1,24 @@
 import { useState } from "react";
 
 const Event = ({ event }) => {
-const [showDetails, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
-let detailsChoice = showDetails ? "Hide Details" : "Show Details";
+  let detailsChoice = showDetails ? "Hide Details" : "Show Details";
 
-const handleToggleDetails = () => {
+  const handleToggleDetails = () => {
     setShowDetails(!showDetails);
     detailsChoice = showDetails ? "Hide Details" : "Show Details";
-}
+  };
 
   return (
     <li className="event">
       <h2>{event.summary}</h2>
       <p>{event.created}</p>
       <p className="city">{event.location}</p>
-        <button
-        className="details-btn"
-        onClick={() => handleToggleDetails()}
-        >{detailsChoice}</button>
-        {showDetails && 
-        <div className="details"></div>
-        }
+      {showDetails && <div className="details">{event.description}</div>}
+      <button className="details-btn" onClick={() => handleToggleDetails()}>
+        {detailsChoice}
+      </button>
     </li>
   );
 };
